c59a1136f74556d0cd41ff2aa5dd10777f0c3c12
Tomek <soltom6@gmail.com>
2019-05-05 22:19:48 +0200

    Added attribute selection function, WIP attr reset & forward buttons; added info elements in race & class panels

--- main.js
+++ main.js
@@ -8,21 +8,30 @@ summaryGender = document.querySelector('p.summaryGender span');
 summaryRace = document.querySelector('p.summaryRace span');
 summaryClass = document.querySelector('p.summaryClass span');
 summaryPortrait = document.querySelector('img.summaryPortrait');
+summaryStrength = document.getElementById('summaryStrength');
+summaryDexterity = document.getElementById('summaryDexterity');
+summaryConstitution = document.getElementById('summaryConstitution');
+summaryIntelligence = document.getElementById('summaryIntelligence');
+summaryCharisma = document.getElementById('summaryCharisma');
+summaryWillpower = document.getElementById('summaryWillpower');
 
 resetGenderButton = document.querySelector('button.resetGender');
 resetRaceButton = document.querySelector('button.resetRace');
 resetNameButton = document.querySelector('button.resetName');
 resetClassButton = document.querySelector('button.resetClass');
 resetPortraitButton = document.querySelector('button.resetPortrait');
+resetAttributes = document.querySelector('button.resetAttributes');
 
 genderForward = document.querySelector('button.genderForward');
 raceForward = document.querySelector('button.raceForward');
 nameForward = document.querySelector('button.nameForward');
 classForward = document.querySelector('button.classForward');
 portraitForward = document.querySelector('button.portraitForward');
+attributeForward = document.querySelector('button.attributeForward');
 
 btnGender = document.querySelectorAll('button.gender');
 btnRace = document.querySelectorAll('.charRace');
+inputName = document.getElementById('inputName');
 portraitSelect = document.querySelectorAll('.portraits button');
 
 genderPanel = document.querySelector('div.genderSelection');
@@ -38,11 +47,18 @@ nameValue = document.querySelector('.nameSelected');
 classType = document.querySelector('.classSelected');
 attributeValue = document.querySelectorAll('.attribute span')
 
+portraits = document.getElementById('portraits');
 portrait1 = document.getElementById('portrait1');
 portrait2 = document.getElementById('portrait2');
 portrait3 = document.getElementById('portrait3');
 portrait4 = document.getElementById('portrait4');
 
+strValue = document.getElementById('strValue');
+dexValue = document.getElementById('dexValue');
+conValue = document.getElementById('conValue');
+intValue = document.getElementById('intValue');
+chaValue = document.getElementById('chaValue');
+willValue = document.getElementById('willValue');
 pointsLeft = document.querySelector('.pointsLeft span');
 
 //--RESET FUNCTIONS--//
@@ -85,7 +101,7 @@ const resetRace = (e) => {
     for (let i = 0; i < activeRace.length; i++) {
         activeRace[i].classList.remove('active');
     }
-}
+};
 const resetName = (e) => {
     e.preventDefault();
     nameValue.textContent = "";
@@ -97,7 +113,10 @@ const resetName = (e) => {
     raceForward.classList.remove('hidden');
     genderPanel.classList.remove('hidden');
     classPanel.classList.add('hidden');
-}
+    document.getElementById('inputName').disabled = !true;
+    document.getElementById('submitName').disabled = !true;
+
+};
 const resetClass = (e) => {
     e.preventDefault();
     classType.textContent = "";
@@ -106,7 +125,7 @@ const resetClass = (e) => {
     classForward.classList.add('hidden');
     summaryClass.textContent = '';
     resetNameButton.classList.remove('hidden');
-    portraitSelection.classList.add('hidden');
+    portraitPanel.classList.add('hidden');
     document.getElementById('warrior').disabled = !true;
     document.getElementById('wizard').disabled = !true;
     document.getElementById('cleric').disabled = !true;
@@ -119,8 +138,7 @@ const resetClass = (e) => {
     for (let i = 0; i < activeClass.length; i++) {
         activeClass[i].classList.remove('active');
     }
-}
-
+};
 const resetPortrait = (e) => {
     e.preventDefault();
     summaryPortrait.classList.add('hidden');
@@ -138,7 +156,12 @@ const resetPortrait = (e) => {
     for (let i = 0; i < activePortrait.length; i++) {
         activePortrait[i].classList.remove('active');
     }
-}
+};
+
+resetAttributes.addEventListener('click', (e) => {
+    e.preventDefault();
+
+});
 
 //--FORWARD FUNCTIONS--//
 
@@ -174,6 +197,20 @@ portraitForward.addEventListener('click', (e) => {
     attributeSelect();
 });
 
+attributeForward.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (pointsLeft.textContent === '0') {
+        summaryStrength.textContent = strValue.textContent;
+        summaryDexterity.innerHTML = dexValue.textContent;
+        summaryConstitution.innerHTML = conValue.textContent;
+        summaryIntelligence.innerHTML = intValue.textContent;
+        summaryCharisma.innerHTML = chaValue.textContent;
+        summaryWillpower.innerHTML = willValue.textContent;
+    } else {
+        alert('You have points left to spend!');
+    }
+});
+
 //--MAIN FUNCTIONS--
 document.body.querySelector('.start').addEventListener('click', startGame = (e) => {
     e.preventDefault();
@@ -181,7 +218,6 @@ document.body.querySelector('.start').addEventListener('click', startGame = (e) 
     summary.classList.remove('hidden');
     genderPanel.classList.remove('hidden');
 });
-const selected = document.getElementsByClassName('selected');
 
 btnGender.forEach((gender) => gender.addEventListener('click', function genderSelect(e) {
     if (e.target.classList.contains('gender')) {
@@ -220,42 +256,51 @@ document.body.querySelector('.raceSelection').addEventListener('click', function
             raceType.textContent = `You chose a human.`;
             raceType.style.backgroundColor = 'rgb(75, 19, 19)';
             summaryRace.textContent = 'Human';
+            chaValue.innerHTML = parseInt(chaValue.innerHTML) + 1;
+            willValue.innerHTML = parseInt(willValue.innerHTML) - 1;
         }
         if (e.target.classList.contains('elf')) {
             raceType.textContent = `You chose an elf.`;
             raceType.style.backgroundColor = 'rgb(75, 19, 19)';
             summaryRace.textContent = 'Elf';
+            intValue.innerHTML = parseInt(intValue.innerHTML) + 1;
+            conValue.innerHTML = parseInt(conValue.innerHTML) - 1;
         }
         if (e.target.classList.contains('dwarf')) {
             raceType.textContent = `You chose a dwarf.`;
             raceType.style.backgroundColor = 'rgb(75, 19, 19)';
             summaryRace.textContent = 'Dwarf';
+            conValue.innerHTML = parseInt(conValue.innerHTML) + 1;
+            dexValue.innerHTML = parseInt(dexValue.innerHTML) - 1;
+
         }
         if (e.target.classList.contains('halfling')) {
             raceType.textContent = `You chose a halfling.`;
             raceType.style.backgroundColor = 'rgb(75, 19, 19)';
             summaryRace.textContent = 'Halfling';
+            dexValue.innerHTML = parseInt(dexValue.innerHTML) + 1;
+            strValue.innerHTML = parseInt(strValue.innerHTML) - 1;
         }
     }
 });
 
 document.body.querySelector('.submitName').addEventListener('click', function nameSelect(e) {
     e.preventDefault();
-    const input = document.querySelector('input');
-    const userNameSelection = input.value;
-    if (userNameSelection) {
+    const userNameSelection = inputName.value;
+    if (typeof userNameSelection === 'string') {
         nameValue.textContent = `You'll be known as ${userNameSelection}!`;
         nameValue.style.backgroundColor = 'rgb(75, 19, 19)';
-        input.value = "";
+        inputName.value = "";
         resetNameButton.classList.remove('hidden');
         nameForward.classList.remove('hidden');
         summaryName.textContent = userNameSelection;
         resetRaceButton.classList.add('hidden');
         raceForward.classList.add('hidden');
+        document.getElementById('inputName').disabled = true;
+        document.getElementById('submitName').disabled = true;
     }
-    if (typeof userNameSelection === 'number' || !userNameSelection) {
+    if (typeof userNameSelection === 'number' || userNameSelection.length == 0) {
         alert('Input name!');
-        nameValue.textContent = "";
     }
 });
 
@@ -274,22 +319,30 @@ document.body.querySelector('.classSelection').addEventListener('click', functio
         if (e.target.classList.contains('warrior')) {
             classType.textContent = `You chose a Warrior class!`;
             classType.style.backgroundColor = 'rgb(75, 19, 19)';
-            summaryClass.textContent = 'Warrior'
+            summaryClass.textContent = 'Warrior';
+            strValue.innerHTML = parseInt(strValue.innerHTML) + 1;
+            conValue.innerHTML = parseInt(conValue.innerHTML) + 1;
         }
         if (e.target.classList.contains('wizard')) {
             classType.textContent = `You chose a Wizard class!`;
             classType.style.backgroundColor = 'rgb(75, 19, 19)';
             summaryClass.textContent = 'Wizard';
+            intValue.innerHTML = parseInt(intValue.innerHTML) + 1;
+            willValue.innerHTML = parseInt(willValue.innerHTML) + 1;
         }
         if (e.target.classList.contains('cleric')) {
             classType.textContent = `You chose a Cleric class!`;
             classType.style.backgroundColor = 'rgb(75, 19, 19)';
             summaryClass.textContent = 'Cleric';
+            chaValue.innerHTML = parseInt(chaValue.innerHTML) + 1;
+            willValue.innerHTML = parseInt(willValue.innerHTML) + 1;
         }
         if (e.target.classList.contains('rogue')) {
             classType.textContent = `You chose a Rogue class!`;
             classType.style.backgroundColor = 'rgb(75, 19, 19)';
             summaryClass.textContent = 'Rogue';
+            dexValue.innerHTML = parseInt(dexValue.innerHTML) + 1;
+            intValue.innerHTML = parseInt(intValue.innerHTML) + 1;
         }
     }
 });
@@ -299,18 +352,23 @@ const portraitSelection = () => {
     const img2 = new Image();
     const img3 = new Image();
     const img4 = new Image();
+
     img1.onload = function () {
         portrait1.appendChild(img1);
     };
+
     img2.onload = function () {
         portrait2.appendChild(img2);
     };
+
     img3.onload = function () {
         portrait3.appendChild(img3);
     };
+
     img4.onload = function () {
         portrait4.appendChild(img4);
     };
+
     if (summaryGender.textContent === 'Female' && summaryRace.textContent === 'Human') {
         img1.src = 'img/portraits/Human/Female/humFemale01.bmp';
         img2.src = 'img/portraits/Human/Female/humFemale02.bmp';
@@ -359,7 +417,7 @@ const portraitSelection = () => {
         img3.src = 'img/portraits/Halfling/Male/halfMale03.bmp';
         img4.src = 'img/portraits/Halfling/Male/halfMale04.bmp';
     }
-}
+};
 
 portraitSelect.forEach((portrait) => portrait.addEventListener('click', function portraitSelect(e) {
     resetPortraitButton.classList.remove('hidden');
@@ -376,89 +434,89 @@ portraitSelect.forEach((portrait) => portrait.addEventListener('click', function
 
 const attributeSelect = () => {
     document.getElementById('strPlus').addEventListener('click', () => {
-        if (parseInt(pointsLeft.innerHTML) > 0 && document.getElementById('strValue').innerHTML < '20') {
-            document.getElementById('strValue').innerHTML = parseInt(document.getElementById('strValue').innerHTML) + 1;
+        if (parseInt(pointsLeft.innerHTML) > 0 && strValue.innerHTML < 20) {
+            strValue.innerHTML = parseInt(strValue.innerHTML) + 1;
             pointsLeft.innerHTML = parseInt(pointsLeft.innerHTML) - 1;
         }
     });
 
     document.getElementById('strMinus').addEventListener('click', () => {
         if (parseInt(document.getElementById('strValue').innerHTML) > 3) {
-            document.getElementById('strValue').innerHTML = parseInt(document.getElementById('strValue').innerHTML) - 1;
+            strValue.innerHTML = parseInt(strValue.innerHTML) - 1;
             pointsLeft.innerHTML = parseInt(pointsLeft.innerHTML) + 1;
         }
     });
 
     document.getElementById('dexPlus').addEventListener('click', () => {
-        if (parseInt(pointsLeft.innerHTML) > 0 && document.getElementById('dexValue').innerHTML < '20') {
-            document.getElementById('dexValue').innerHTML = parseInt(document.getElementById('dexValue').innerHTML) + 1;
+        if (parseInt(pointsLeft.innerHTML) > 0 && document.getElementById('dexValue').innerHTML < 20) {
+            dexValue.innerHTML = parseInt(dexValue.innerHTML) + 1;
             pointsLeft.innerHTML = parseInt(pointsLeft.innerHTML) - 1;
         }
     });
 
     document.getElementById('dexMinus').addEventListener('click', () => {
         if (parseInt(document.getElementById('dexValue').innerHTML) > 3) {
-            document.getElementById('dexValue').innerHTML = parseInt(document.getElementById('dexValue').innerHTML) - 1;
+            dexValue.innerHTML = parseInt(dexValue.innerHTML) - 1;
             pointsLeft.innerHTML = parseInt(pointsLeft.innerHTML) + 1;
         }
     });
 
     document.getElementById('conPlus').addEventListener('click', () => {
-        if (parseInt(pointsLeft.innerHTML) > 0 && document.getElementById('conValue').innerHTML < '20') {
-            document.getElementById('conValue').innerHTML = parseInt(document.getElementById('conValue').innerHTML) + 1;
+        if (parseInt(pointsLeft.innerHTML) > 0 && document.getElementById('conValue').innerHTML < 20) {
+            conValue.innerHTML = parseInt(conValue.innerHTML) + 1;
             pointsLeft.innerHTML = parseInt(pointsLeft.innerHTML) - 1;
         }
     });
 
     document.getElementById('conMinus').addEventListener('click', () => {
         if (parseInt(document.getElementById('conValue').innerHTML) > 3) {
-            document.getElementById('conValue').innerHTML = parseInt(document.getElementById('conValue').innerHTML) - 1;
+            conValue.innerHTML = parseInt(conValue.innerHTML) - 1;
             pointsLeft.innerHTML = parseInt(pointsLeft.innerHTML) + 1;
         }
     });
 
     document.getElementById('intPlus').addEventListener('click', () => {
-        if (parseInt(pointsLeft.innerHTML) > 0 && document.getElementById('intValue').innerHTML < '20') {
-            document.getElementById('intValue').innerHTML = parseInt(document.getElementById('intValue').innerHTML) + 1;
+        if (parseInt(pointsLeft.innerHTML) > 0 && document.getElementById('intValue').innerHTML < 20) {
+            intValue.innerHTML = parseInt(intValue.innerHTML) + 1;
             pointsLeft.innerHTML = parseInt(pointsLeft.innerHTML) - 1;
         }
     });
 
     document.getElementById('intMinus').addEventListener('click', () => {
         if (parseInt(document.getElementById('intValue').innerHTML) > 3) {
-            document.getElementById('intValue').innerHTML = parseInt(document.getElementById('intValue').innerHTML) - 1;
+            intValue.innerHTML = parseInt(intValue.innerHTML) - 1;
             pointsLeft.innerHTML = parseInt(pointsLeft.innerHTML) + 1;
         }
     });
 
     document.getElementById('chaPlus').addEventListener('click', () => {
-        if (parseInt(pointsLeft.innerHTML) > 0 && document.getElementById('chaValue').innerHTML < '20') {
-            document.getElementById('chaValue').innerHTML = parseInt(document.getElementById('chaValue').innerHTML) + 1;
+        if (parseInt(pointsLeft.innerHTML) > 0 && document.getElementById('chaValue').innerHTML < 20) {
+            chaValue.innerHTML = parseInt(chaValue.innerHTML) + 1;
             pointsLeft.innerHTML = parseInt(pointsLeft.innerHTML) - 1;
         }
     });
 
     document.getElementById('chaMinus').addEventListener('click', () => {
         if (parseInt(document.getElementById('chaValue').innerHTML) > 3) {
-            document.getElementById('chaValue').innerHTML = parseInt(document.getElementById('chaValue').innerHTML) - 1;
+            chaValue.innerHTML = parseInt(chaValue.innerHTML) - 1;
             pointsLeft.innerHTML = parseInt(pointsLeft.innerHTML) + 1;
         }
     });
 
     document.getElementById('willPlus').addEventListener('click', () => {
-        if (parseInt(pointsLeft.innerHTML) > 0 && document.getElementById('willValue').innerHTML < '20') {
-            document.getElementById('willValue').innerHTML = parseInt(document.getElementById('willValue').innerHTML) + 1;
+        if (parseInt(pointsLeft.innerHTML) > 0 && document.getElementById('willValue').innerHTML < 20) {
+            willValue.innerHTML = parseInt(willValue.innerHTML) + 1;
             pointsLeft.innerHTML = parseInt(pointsLeft.innerHTML) - 1;
         }
     });
 
     document.getElementById('willMinus').addEventListener('click', () => {
         if (parseInt(document.getElementById('willValue').innerHTML) > 3) {
-            document.getElementById('willValue').innerHTML = parseInt(document.getElementById('willValue').innerHTML) - 1;
+            willValue.innerHTML = parseInt(willValue.innerHTML) - 1;
             pointsLeft.innerHTML = parseInt(pointsLeft.innerHTML) + 1;
         }
     });
-}
+};
 
 document.body.querySelector('.resetGender').addEventListener('click', resetGender);
 document.body.querySelector('.resetRace').addEventListener('click', resetRace);
